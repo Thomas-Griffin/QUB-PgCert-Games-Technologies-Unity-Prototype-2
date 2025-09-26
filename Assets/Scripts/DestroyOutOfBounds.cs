61933f57a617f35bd68b827fa9c87ad6b2eadcda
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-26 11:45:10 +0100

    refactor DestroyOutOfBounds script to use constants for bounds and simplify condition

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30;
-    private float lowerBound = -10;
+    private const float TopBound = 30;
+    private const float LowerBound = -10;
 
 
     // Update is called once per frame
     void Update()
     {
         // Destroy the object if it goes out of bounds
-        if (transform.position.z > topBound || transform.position.z < lowerBound)
+        if (transform.position.z is > TopBound or < LowerBound)
         {
             // the object is destroyed
             Destroy(gameObject);
