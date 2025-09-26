7255ad0f7848d367bbb63014bebbfb62e1f40b44
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-26 11:39:14 +0100

    add out-of-bounds destruction logic to DestroyOutOfBounds script and update FoodProjectile prefab to use it

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float topBound = 30;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Destroy the object if it goes out of bounds
+        if (transform.position.z > topBound)
+        {
+            // the object is destroyed
+            Destroy(gameObject);
+        }
     }
 }
