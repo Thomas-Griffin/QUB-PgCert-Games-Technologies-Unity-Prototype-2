228cecd49765b06744e19689d4076265775d4b2f
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-26 16:18:31 +0100

    add animal prefab spawning logic to SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+                animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
