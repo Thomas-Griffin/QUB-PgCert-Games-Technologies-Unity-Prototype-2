1039c1289aeb853c7255df2e271ed0855aa61ef2
Thomas Griffin <40271229+Thomas-Griffin@users.noreply.github.com>
2025-09-26 11:43:16 +0100

    add DestroyOutOfBounds component to Animal prefabs and update bounds logic

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
 
 
     // Update is called once per frame
     void Update()
     {
         // Destroy the object if it goes out of bounds
-        if (transform.position.z > topBound)
+        if (transform.position.z > topBound || transform.position.z < lowerBound)
         {
             // the object is destroyed
             Destroy(gameObject);
